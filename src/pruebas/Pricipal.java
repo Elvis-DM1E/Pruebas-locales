caea28891cd71186e86d532163cdf159f5908def
elvis <edanciu@educa.madrid.org>
2025-02-25 19:26:54 +0100

    segundo commit

--- src/pruebas/Pricipal.java
+++ src/pruebas/Pricipal.java
@@ -5,5 +5,7 @@ package pruebas;
 public class Pricipal {
     public static void main(String[] args) {
         System.out.println("Estamos haciendo una prueba, aprendiendo Git");
+        
+        ComoQuieras.pintarMensaje("Otro mensaje");
     }
 }
